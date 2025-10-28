bf7b8788a3571b22fdf5597808a701e9c8f8816d
umanema <dn.antropov@gmail.com>
2025-10-28 14:33:32 +0100

    add size to wallbuildbutton

--- game/scripts/ui/WallBuildButton.cs
+++ game/scripts/ui/WallBuildButton.cs
@@ -6,10 +6,14 @@ public partial class WallBuildButton : BuildButton
 
     [Export]
     WallEmitter.WallType wallType;
+
+    [Export]
+    int _size = 9;
     protected override void OnButtonReleased()
     {
         base.OnButtonReleased();
         WallEmitter wallEmitter = (WallEmitter)worldObject;
         wallEmitter.wallType = wallType;
+        wallEmitter.GridSize = new Vector2I(_size, _size);
     }
 }
